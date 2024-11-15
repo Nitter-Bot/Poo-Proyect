4c31b6075d4eb0645affb3e76cbf21890b6ca064
Juan_Velazco <107668129+Juanillo101018@users.noreply.github.com>
2024-11-15 06:23:56 -0600

    Update Vendedor.java
    Clase vendedor. Metodos pendientes por implementar y sujetos a cambios.

---
+++ Cliente/Vendedor.java
@@ -0,0 +1,69 @@
+package Cliente;
+import Acceso.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class vendedor{
+  private String nombre;
+  private int idTrabajador;
+  private String jefeDirecto;
+  private List<Vuelo> vuelosDisponibles;
+  private ControlUsuarios controlUsuarios;
+
+  //Constructor :D
+  public Vendedor(String nombre, int idTrabajador, String jefeDirecto, ControlUsuarios controlUsuarios) {
+    this.nombre = nombre;
+    this.idTrabajador = idTrabajador;
+    this.jefeDirecto = jefeDirecto;
+    this.vuelosDisponibles = new ArrayList<>();
+    this.controlUsuarios = controlUsuarios;
+  }
+  
+  public String getNombre(){
+    return nombre;
+  }
+  
+  public void setNombre(String nombre){
+    this.nombre = nombre;
+  }
+
+  public int getIdTrabajador(){
+    return idTrabajador;
+  }
+
+  public void setIdTrabajador(int idTrabajador){
+    this.idTranajador = idTrabajador;
+  }
+
+  //Autenticacion para el vendedor. (Sujeto a cambios)
+  public boolean iniciarSesion(String usuario, String password){
+    Usuario user = controlUsuarios.accessGranted(usuario, password);
+    if(user != null){
+      System.out.println("Inicio de secsion exitoso para: " + user.getUser());
+      return true;
+    }else{
+      System.out.println("Error de autenticacion.");
+      return false;
+    }
+  }
+  
+  public void verVuelosDisponibles(){
+    System.out.println("Vuelos disponibles:");
+    for(Vuelo vuelo : vuelosDisponibles){
+      System.out.println(vuelo);
+    }
+  }
+  
+  public void asignarAsientos(Vuelo vuelo, int numeroAsiento){
+    //Pendiente
+  }
+
+  public void quitarAsientos(Vuelo vuelo, int numeroAsiento){
+    //Pendiente
+  }  
+
+  public void generarReciboTicket(Vuelo vuelo){
+    //Pendiente
+  }
+
+}
