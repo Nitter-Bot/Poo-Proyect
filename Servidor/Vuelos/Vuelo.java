a03cb46245da3ce82e70a2abb6957bea7a40e1e9
JonOvando <jonathandanielovando@gmail.com>
2024-11-19 14:19:40 -0600

    Sistema_Vuelo.java
    Ya esta terminado.

--- Servidor/Vuelos/Vuelo.java
+++ Servidor/Vuelos/Vuelo.java
@@ -1,13 +1,25 @@
-import java.HashSet;
-import java.Set;
+import java.util.HashSet;
+import  java.util.Set;
+
 class Vuelo {
-  Set<int>asientos= new HashSet<>();
-  
+  Set<String>asientos= new HashSet<>();//En el set unicamente contiene los asientos disponibles
   int id;
   double costo;
   String origen,destino,fechaSalida;
-  final int capacidad;//Por definir la capacidad del avion
+  final int capacidad=50;//Aun no entiendo su funcion pero ya esta declarada jaja
+   
     Vuelo (){}
+  //Siempre al crear un objeto Vuelo, se inicializa el set con los asientos
+    Vuelo(){
+      for (int i=0; i<10;i++){
+        asientos.add("A"+String.valueOf(i));
+        asientos.add("B"+String.valueOf(i));
+        asientos.add("C"+String.valueOf(i));
+        asientos.add("D"+String.valueOf(i));
+        asientos.add("E"+String.valueOf(i));
+      }
+    }
+  //CONSTRUCTORES
     Vuelo(int id,double costo,String origen,String destino,String fechaSalida){
       this.id=id;
       this.costo=costo;
@@ -15,34 +27,50 @@ class Vuelo {
       this.destino=destino;
       this.fechaSalida=fechaSalida;
     }
+
   Vuelo(int id,double costo, String fechaSalida){
       this.id=id;
       this.costo=costo;
       this.fechaSalida=fechaSalida;
   }
+//METODOS
   void asignarAsiento(String lugar){
     if(asientos.contains(lugar)){
       asientos.remove(lugar);
+      System.out.println("Se asigno el asiento : "+lugar);
     }else {
-      System.out.println("El asiento que eligio no esta disponible. ");
+      System.out.println("El asiento : "+lugar+" no esta disponible. ");
     }    
   }
+  
   void desocuparAsiento(String lugar){
     if(asientos.contains(lugar)){
-      System.out.println("El asiento aun no ha sido elegido. ");
+      System.out.println("El asiento : "+lugar+" aun no ha sido elegido. ");
     }else {
       asientos.add(lugar);
-      System.out.println("El asiento ha sido desocupado. ");
+      System.out.println("El asiento: "+lugar+" ha sido desocupado. ");
     }
   }
   
   void mostrarAsientosDisponibles(){
      if (asientos.isEmpty()){
-       System.out.println("Asientos Disponibles"+asientos);
-     }else{
        System.out.println("Sin asientos disponibles");
+     }else{
+       System.out.println("Asientos Disponibles"+asientos);
        
      }
+    
   }
+
+
 }
  
+/*class Controladora {
+  public static void main (String [] a) {
+
+      Vuelo p1 = new Vuelo(1234324,123.23,"CDMX","ACAPULCO","12/04/2024");
+      p1.asignarAsiento("A2");
+      p1.mostrarAsientosDisponibles();
+      p1.desocuparAsiento("A8");
+  }
+}*/
